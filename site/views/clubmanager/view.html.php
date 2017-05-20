9198393ceb980168f8bc1b46ed45feb120dee084
David Shannon <dzshanno@yahoo.com>
2017-05-20 21:01:51 +0100

    implement the view

---
+++ site/views/clubmanager/view.html.php
@@ -0,0 +1,35 @@
+	<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_clubmanager
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * HTML View class for the clubmanager Component
+ *
+ * @since  0.0.2
+ */
+class clubmanagerViewclubmanager extends JViewLegacy
+{
+	/**
+	 * Display the clubmanager view
+	 *
+	 * @param   string  $tpl  The name of the template file to parse; automatically searches through the template paths.
+	 *
+	 * @return  void
+	 */
+	function display($tpl = null)
+	{
+		// Assign data to the view
+		$this->msg = 'Hello ClubManager';
+ 
+		// Display the view
+		parent::display($tpl);
+	}
+}
