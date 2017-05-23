a07fcb5c4a1f1c3874919257091f32f71f2edb89
David Shannon <dzshanno@yahoo.com>
2017-05-23 23:08:23 +0100

    update admin views

---
+++ admin/models/clubmanager.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_clubmanager
+ *
+ * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+/**
+ * clubmanagerList Model
+ *
+ * @since  0.0.1
+ */
+class clubmanagerModelclubmanager extends JModelList
+{
+	/**
+	 * Method to build an SQL query to load the list data.
+	 *
+	 * @return      string  An SQL query
+	 */
+	protected function getListQuery()
+	{
+		// Initialize variables.
+		$db    = JFactory::getDbo();
+		$query = $db->getQuery(true);
+ 
+		// Create the base select statement.
+		$query->select('*')
+                ->from($db->quoteName('#__clubmanager'));
+ 
+		return $query;
+	}
+}
