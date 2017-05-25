1553a2216baab6c030383eb15c9a133386028d47
David Shannon <dzshanno@yahoo.com>
2017-05-25 23:04:42 +0100

    access and config.xml

--- admin/clubmanager.php
+++ admin/clubmanager.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  com_helloworld
+ * @subpackage  com_clubmanager
  *
  * @copyright   Copyright (C) 2005 - 2017 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
@@ -9,6 +9,10 @@
  
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
+
+if (!JFactory::getUser()->authorise('core.manage','com_clubmanager'))
+{return JError::raiseWarning(404,JText::_('JERROR_ALERTNOAUTHOR'))
+}
  
 // Get an instance of the controller prefixed by ClubManager
 $controller = JControllerLegacy::getInstance('clubmanager');
