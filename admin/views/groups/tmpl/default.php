3f5520862d16642c6f5cf2e66e7a4cbafc6251e1
David Shannon <dzshanno@yahoo.com>
2017-05-29 14:17:27 +0100

    language and image size changes

--- admin/views/groups/tmpl/default.php
+++ admin/views/groups/tmpl/default.php
@@ -42,7 +42,7 @@ $listDirn  = '';
             </a>
           </td>
 		  <td class="nowrap has-context">
-		    <img src="<?php echo JURI::root(); echo $this->escape($item->grouplogo); ?>" alt = "logo for <?php echo $this->escape($item->groupname); ?>">
+		    <img src="<?php echo JURI::root(); echo $this->escape($item->grouplogo); ?>" alt = "logo for <?php echo $this->escape($item->groupname); ?>" width=100>
               
             </a>
           </td>
