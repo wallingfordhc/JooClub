6bd0a4cc8e900f9e3e2deb49c3ae61904c108ffd
David Shannon <dzshanno@yahoo.com>
2017-05-30 11:05:25 +0100

    Update matchs.php
    Model should be match not matchs

--- admin/controllers/matchs.php
+++ admin/controllers/matchs.php
@@ -3,7 +3,7 @@ defined('_JEXEC') or die;
 
 class clubmanagerControllermatchs extends JControllerAdmin
 {
-  public function getModel($name = 'matchs', $prefix = 'clubmanagerModel', $config = array('ignore_request' => true))
+  public function getModel($name = 'match', $prefix = 'clubmanagerModel', $config = array('ignore_request' => true))
   {
     $model = parent::getModel($name, $prefix, $config);
     return $model;
