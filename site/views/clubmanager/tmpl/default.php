bf38e23a942eb3f74a619c363bf984b2ba7b0025
David Shannon <dzshanno@yahoo.com>
2017-05-31 20:26:48 +0100

    remove table headers

--- site/views/clubmanager/tmpl/default.php
+++ site/views/clubmanager/tmpl/default.php
@@ -3,33 +3,7 @@ defined('_JEXEC') or die;
 ?>
 
 <table class="table table-striped" id="matchList">
-      <thead>
-        <tr>
-          <th width="1%" class="hidden-phone">
-            <input type="checkbox" name="checkall-toggle" value="" title="<?php echo JText::_('JGLOBAL_CHECK_ALL'); ?>" onclick="Joomla.checkAll(this)" />
-          </th>
-		  
-          <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_HOMETEAM_TITLE', 'hometeamname', $listDirn, $listOrder); ?>
-          </th>
-		  <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_HOMETEAM_SCORE', 'homescore', $listDirn, $listOrder); ?>
-          </th>
-		  <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_AWAYTEAM_SCORE', 'awayscore', $listDirn, $listOrder); ?>
-          </th>
-		  <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_AWAYTEAM_TITLE', 'awayteamname', $listDirn, $listOrder); ?>
-          </th>
-		  <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_LOCATION_TITLE', 'location', $listDirn, $listOrder); ?>
-          </th>
-		  <th class="title">
-            <?php echo JHtml::_('grid.sort', 'COM_CLUBMANAGER_matchesTATUS_TITLE', 'status', $listDirn, $listOrder); ?>
-          </th>
-		  
-        </tr>
-      </thead>
+      
       <tbody>
       <?php 
 	  $groupdate=null;
@@ -42,9 +16,6 @@ defined('_JEXEC') or die;
     echo "${row['query']}<br>\n";
         ?>
         <tr class="row<?php echo $i % 2; ?>">
-          <td class="center hidden-phone">
-            <?php echo JHtml::_('grid.id', $i, $item->matchID); ?>
-          </td>
 		  
           <td class="nowrap has-context">
             <a href="<?php echo JRoute::_('index.php?option=com_clubmanager&task=match.edit&matchID='.(int) $item->matchID); ?>">
