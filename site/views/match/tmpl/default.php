00ca7632249ab900abc3aec0b0a069adaa0fdf06
David Shannon <dzshanno@yahoo.com>
2017-06-03 21:50:33 +0100

    more styling

--- site/views/match/tmpl/default.php
+++ site/views/match/tmpl/default.php
@@ -4,27 +4,29 @@
    <?php foreach ($this->items as $item) : ?>
       <div class=cmmatch_result >
          <div class="cmmatch_pushbacktime" >
-            <?php echo $item->pushback; ?>
+            <?php 
+			$displaydate = date_format(datecreate($item->pushback),'jS M h:i');
+			echo $displaydate; ?>
          </div>
          <div class="cmmatch_hometeam">
 		   <span class="cmmatch_teamlogo">
-			<img src="<?php echo $item->hometeamlogo; ?>">
+			<img class="cmmatch_teamlogo" src="<?php echo $item->hometeamlogo; ?>">
            </span>
 		   <span class="cmmatch_hometeamname">
 			<?php echo $item->hometeamname; ?>
 		   </span>
          </div>
+		 <div class="cmmatch_homescore">
+		   <?php echo $item->homescore; ?>
+		 </div>
 		 <div class="cmmatch_awayteam">
 		   <span class="cmmatch_teamlogo">
-			<img src="<?php echo $item->awayteamlogo; ?>">
+			<img class="cmmatch_teamlogo" src="<?php echo $item->awayteamlogo; ?>">
            </span>
 		   <span class="cmmatch_awayteamname">
 			<?php echo $item->awayteamname; ?>
 		   </span>
          </div>
-		 <div class="cmmatch_homescore">
-		   <?php echo $item->homescore; ?>
-		 </div>
 		 <div class="cmmatch_awayscore">
 		   <?php echo $item->awayscore; ?>
 		 </div>
