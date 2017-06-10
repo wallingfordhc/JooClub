b7b125b103f18ef0aa3d1555656d2fb8852aae8e
David Shannon <dzshanno@yahoo.com>
2017-06-10 11:06:37 +0100

    added gender and memberID

--- admin/views/player/tmpl/edit.php
+++ admin/views/player/tmpl/edit.php
@@ -19,6 +19,15 @@ defined('_JEXEC') or die;
         </div>
 		
 		<div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('gender'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('gender'); ?></div>
+        </div>
+		<div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('memberID'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('memberID'); ?></div>
+        </div>
+
+		<div class="control-group">
           <div class="control-label"><?php echo $this->form->getLabel('DOB'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('DOB'); ?></div>
         </div>
