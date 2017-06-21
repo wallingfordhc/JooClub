0306a2b04402a52f679f146ddc60352e1fe7e694
David Shannon <dzshanno@yahoo.com>
2017-06-21 22:52:18 +0100

    adding attendance front end form

---
+++ site/models/attendance.php
@@ -0,0 +1,44 @@
+<?php
+defined('_JEXEC') or die;
+
+class clubmanagerModelattendance extends JModelAdmin
+{
+  protected $text_prefix = 'COM_CLUBMANAGER';
+
+  public function getTable($type = 'attendnace', $prefix = 'clubmanagerTable', $config = array())
+  {
+    return JTable::getInstance($type, $prefix, $config);
+  }
+
+  public function getForm($data = array(), $loadData = true)
+  {
+    $app = JFactory::getApplication();
+
+    $form = $this->loadForm('com_clubmanager.attendance', 'attendance', array('control' => 'jform', 'load_data' => $loadData));
+    if (empty($form))
+    {
+      return false;
+    }
+
+    return $form;
+  }
+
+  protected function loadFormData()
+  {
+    $data = JFactory::getApplication()->getUserState('com_clubmanager.edit.attendance.data', array());
+
+    if (empty($data))
+    {
+      $data = $this->getItem();
+    }
+
+    return $data;
+  }
+
+  protected function prepareTable($table)
+  {
+    $table->hometeamID    = htmlspecialchars_decode($table->hometeamID, ENT_QUOTES);
+  }
+
+  
+}
---
+++ site/views/attendance/tmpl/edit.php
@@ -0,0 +1,45 @@
+<?php
+defined('_JEXEC') or die;
+
+?>
+
+<form action="<?php echo JRoute::_('index.php?option=com_clubmanager&layout=edit&matchID='.(int) $this->item->matchID); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
+  <div class="row-fluid">
+    <div class="span10 form-horizontal">
+
+  <fieldset>
+    <?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
+
+      <?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->matchID) ? JText::_('COM_CLUBMANAGER_NEW_MATCH', true) : JText::sprintf('COM_CLUBMANAGER_EDIT_MATCH', $this->item->matchID, true)); ?>
+        <div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('pushback'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('pushback'); ?></div>
+        </div>
+		
+		<div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('hometeamID'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('hometeamID'); ?></div>
+		  <div class="control-label"><?php echo $this->form->getLabel('homescore'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('homescore'); ?></div>
+        </div>
+		<div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('awayteamID'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('awayteamID'); ?></div>
+		  <div class="control-label"><?php echo $this->form->getLabel('awayscore'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('awayscore'); ?></div>
+        </div>
+		<div class="control-group">
+          <div class="control-label"><?php echo $this->form->getLabel('locationID'); ?></div>
+          <div class="controls"><?php echo $this->form->getInput('locationID'); ?></div>
+		</div>
+		
+      <?php echo JHtml::_('bootstrap.endPanel'); ?>
+
+      <input type="hidden" name="task" value="" />
+      <?php echo JHtml::_('form.token'); ?>
+
+    <?php echo JHtml::_('bootstrap.endPane'); ?>
+    </fieldset>
+    </div>
+  </div>
+</form>
