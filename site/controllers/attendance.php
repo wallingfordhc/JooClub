1ab7db965580dd02b859f17fa03450c094e88e8c
David Shannon <dzshanno@yahoo.com>
2017-06-21 23:17:38 +0100

    bugfix

--- site/controllers/attendance.php
+++ site/controllers/attendance.php
@@ -3,4 +3,14 @@ defined('_JEXEC') or die;
 class clubmanagerControllerattendance extends JControllerForm
 {
 
+protected function allowAdd($data = array())
+	{
+		//$user		= JFactory::getUser();
+		//$categoryId	= JArrayHelper::getValue($data, 'catid', $this->input->getInt('catid'), 'int');
+		$allow		= true;
+	
+		return $allow;
+		
+	}
+
 }
