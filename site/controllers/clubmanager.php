c1c10f6ff3e06753b2744f45a2796b53a991c05d
David Shannon <dzshanno@yahoo.com>
2017-07-04 10:52:51 +0100

    restrict access to  player view

--- site/controllers/clubmanager.php
+++ site/controllers/clubmanager.php
@@ -3,4 +3,5 @@ defined('_JEXEC') or die;
 
 class clubmanagerControllerclubmanager extends JControllerForm
 {
+require_once JPATH_COMPONENT.'helpers/access.php';
 }
---
+++ site/helpers/access.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_clubmanager
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+ 
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// provide a list of person_ids the given user can access
+// uses the __cmconsent table which should contain a set of links between personID and CMS_userID
+
+class clubmanageraccess
+
+function personlist($userID)
+{
+
+ 
+ // if no personID sent to the function set it to be the current user
+	if (empty($userID))
+	{
+	$userID = JFactory::getUser();
+	}
+
+	$db    = $this->getDbo();
+    $query  = $db->getQuery(true);
+
+    $query
+	  ->select($db->quoteName('c.personID','personID'))
+	  ->from($db->quoteName('#__cmconsent').' AS c')
+	  ->where('(c.cmsuserID LIKE '.$userID.')');
+	
+	return $query;
+
+}
