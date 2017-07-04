3b634960b2278640c9d2849db116c2c2b48b3386
David Shannon <dzshanno@yahoo.com>
2017-07-04 16:47:19 +0100

    bugfix

--- site/views/players/view.html.php
+++ site/views/players/view.html.php
@@ -1,14 +1,14 @@
 <?php
 defined('_JEXEC') or die;
 
-class clubmanagerViewmatches extends JViewLegacy
+class clubmanagerViewplayers extends JViewLegacy
 {
   protected $items;
   protected $state;
 
   public function display($tpl = null)
   {
-    $this->items    = $this->get('Items');
+    $this->items = $this->get('Items');
 	$this->state = $this->get('State');
 
     if (count($errors = $this->get('Errors')))
@@ -17,34 +17,9 @@ class clubmanagerViewmatches extends JViewLegacy
       return false;
     }
 
-    $this->addToolbar();
+ 
     parent::display($tpl);
   }
 
-  protected function addToolbar()
-  {
-    $canDo  = clubmanagerHelper::getActions();
-    $bar = JToolBar::getInstance('toolbar');
-
-    JToolbarHelper::title(JText::_('COM_CLUBMANAGER_MANAGER_matches'), '');
-
-    JToolbarHelper::addNew('match.add');
-
-    if ($canDo->get('core.edit'))
-    {
-      JToolbarHelper::editList('match.edit');
-    }
-	if ($canDo->get('core.delete'))
-    {
-      JToolBarHelper::deleteList('Are you sure?', 'matches.delete', 'JTOOLBAR_DELETE');
-    }
-    if ($canDo->get('core.admin'))
-    {
-      JToolbarHelper::preferences('com_clubmanager');
-    }
-
-	// testing adding another toolbar button
-	JToolBarHelper::custom('matches.finalscore', 'finalscore.png', 'finalscore_f2.png', 'Set Final Score', true);
-  }
   
 }
