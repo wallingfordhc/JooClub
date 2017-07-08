f59f433f0ae92bb503ac0ed57d29e7efed208653
David Shannon <dzshanno@yahoo.com>
2017-07-08 18:26:14 +0100

    bugfix

--- site/views/player/view.html.php
+++ site/views/player/view.html.php
@@ -40,14 +40,14 @@ class clubmanagerViewplayer extends JViewLegacy
   }
 
   private function includeAdminEnv()
- +	{
- +		// load the language files for the admin messages as well
- +		$language	= JFactory::getLanguage();
- +		$language->load('joomla', JPATH_ADMINISTRATOR, null, true);
- +		$language->load('com_clubmanager', JPATH_ADMINISTRATOR, null, true);
- +
- +		JLoader::register('JToolBarHelper',   JPATH_ADMINISTRATOR . '/includes/toolbar.php');
- +		JLoader::register('JSubMenuHelper',   JPATH_ADMINISTRATOR . '/includes/subtoolbar.php');
- +		JLoader::register('clubmanagerHelper', JPATH_COMPONENT_ADMINISTRATOR . '/helpers/clubmanager.php');
+ 	{
+ 		// load the language files for the admin messages as well
+ 		$language	= JFactory::getLanguage();
+ 		$language->load('joomla', JPATH_ADMINISTRATOR, null, true);
+ 		$language->load('com_clubmanager', JPATH_ADMINISTRATOR, null, true);
+ 
+ 		JLoader::register('JToolBarHelper',   JPATH_ADMINISTRATOR . '/includes/toolbar.php');
+ 		JLoader::register('JSubMenuHelper',   JPATH_ADMINISTRATOR . '/includes/subtoolbar.php');
+ 		JLoader::register('clubmanagerHelper', JPATH_COMPONENT_ADMINISTRATOR . '/helpers/clubmanager.php');
   	}
 }
