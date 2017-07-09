86fff36de4d59e3d061ff4b67c1b240ffbe143e2
David Shannon <dzshanno@yahoo.com>
2017-07-09 14:56:35 +0100

    add tabs to player view

--- site/views/player/tmpl/edit.php
+++ site/views/player/tmpl/edit.php
@@ -1,40 +1,28 @@
 <?php
-defined('_JEXEC') or die;
-
+defined('_JEXEC') or die;	
 ?>
-<script>
-  $( function() {
-    $( "#tabs" ).tabs();
-  } );
-  </script>
+
 <form action="<?php echo JRoute::_('index.php?option=com_clubmanager&layout=edit&personID='.(int) $this->item->personID); ?>" method="post" name="adminForm" id="adminForm" class="form-validate" enctype="multipart/form-data">
   <div class="row-fluid">
     <div class="span10 form-horizontal">
 	<?php echo $this->addToolbar(); ?>
-	<div id="tabs">
-  <ul>
-    <li><a href="#tabs-1">normal</a></li>
-    <li><a href="#tabs-2">extra</a></li>
-    <li><a href="#tabs-3">custom</a></li>
-  </ul>
-  <div id="tabs-1">
-  <fieldset>
-    <?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
 
-      <?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->matchID) ? JText::_('COM_CLUBMANAGER_NEW_PLAYER', true) : JText::sprintf('COM_CLUBMANAGER_EDIT_PLAYER', $this->item->personID, true)); ?>
+	    <?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'general')); ?>
+
+
+
+      <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'general', empty($this->item->matchID) ? JText::_('COM_CLUBMANAGER_NEW_PLAYER', true) : JText::sprintf('COM_CLUBMANAGER_EDIT_PLAYER', $this->item->personID, true)); ?>
+        <fieldset>
         <div class="control-group">
           <div class="control-label"><?php echo $this->form->getLabel('firstname'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('firstname'); ?></div>
 		  <div class="control-label"><?php echo $this->form->getLabel('surname'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('surname'); ?></div>
         </div>
-		
 		<div class="control-group">
           <div class="control-label"><?php echo $this->form->getLabel('gender'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('gender'); ?></div>
         </div>
-		
-
 		<div class="control-group">
           <div class="control-label"><?php echo $this->form->getLabel('DOB'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('DOB'); ?></div>
@@ -45,31 +33,29 @@ defined('_JEXEC') or die;
 		  <div class="control-label"><?php echo $this->form->getLabel('email'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('email'); ?></div>
         </div>
-	<div class = "control-group">
-        <div><img src="<?php echo $this->escape($this->item->profileimage_url); ?>" alt = "<?php echo $this->escape($this->item->profileimage_url); ?>" width=100>
+	    <div class="control-group">
+          <div><img src="<?php echo $this->escape($this->item->profileimage_url); ?>" alt = "<?php echo $this->escape($this->item->profileimage_url); ?>" width=100></div>
           <div class="control-label"><?php echo $this->form->getLabel('profileimage_url'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('profileimage_url'); ?></div>
-      </div>
-
-	
-	    <div class=""control-group">
+        </div>
+	    <div class="control-group">
           <div class="control-label"><?php echo $this->form->getLabel('memberID'); ?></div>
           <div class="controls"><?php echo $this->form->getInput('memberID'); ?></div>
         </div>
+
 		<?php $user = JFactory::getUser(); ?>
-
 		<?php echo("<input type='hidden' name='memberID' value='".$user->id."' />"); ?>
-      <?php echo("<input type='hidden' name='oldprofilephotoimage_url' value='".$this->escape($this->item->profileimage_url)."' />"); ?>
-
-
-      <?php echo JHtml::_('bootstrap.endPanel'); ?>
-
-      <input type="hidden" name="task" value="" />
-      <?php echo JHtml::_('form.token'); ?>
-    <?php echo JHtml::_('bootstrap.endPane'); ?>
+        <?php echo("<input type='hidden' name='oldprofilephotoimage_url' value='".$this->escape($this->item->profileimage_url)."' />"); ?>
+        <input type="hidden" name="task" value="" />
+        <?php echo JHtml::_('form.token'); ?>
+	        <?php echo JHtml::_('bootstrap.endTab'); ?>
     </fieldset>
+
+
+	    <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'other', JText::_('COM_CLUBMANAGER_PLAYER_TAB_1_NAME', true)); ?>
+More detail
+	    <?php echo JHtml::_('bootstrap.endTab'); ?>
+	    <?php echo JHtml::_('bootstrap.endTabSet'); ?>
 	</div>
-	</div>
-    </div>
   </div>
 </form>
