dedf94211be70db0152e52ff16af3e6f7ae7300c
David Shannon <dzshanno@yahoo.com>
2017-07-13 23:25:12 +0100

    add person class

---
+++ site/controllers/register.php
@@ -0,0 +1,22 @@
+<?php
+
+// set up register so that if it is a register taker the id gets added to the attendance list
+// if its just a memebr of the public they see a login page
+
+
+// No direct access to this file
+defined('_JEXEC') or die('Restricted access');
+ 
+// import Joomla controller library
+jimport('joomla.application.component.controller');
+ 
+/**
+ * Hello World Component Controller
+ */
+class clubmanagerControllerregister extends JControllerForm 
+{
+  function shout()
+  {
+    echo "<p>THIS IS ME SHOUTING!</p>";
+  }
+}
---
+++ site/views/register/view.html.php
@@ -0,0 +1,57 @@
+<?php
+defined('_JEXEC') or die;
+
+class clubmanagerViewregister extends JViewLegacy
+{
+  protected $item;
+  protected $form;
+
+  public function display($tpl = null)
+  {
+    $this->item    = $this->get('Item');
+    $this->form    = $this->get('Form');
+
+    if (count($errors = $this->get('Errors')))
+    {
+      JError::raiseError(500, implode("\n", $errors));
+      return false;
+    }
+	$this->includeAdminEnv();
+    //$this->addToolbar();
+    parent::display($tpl);
+  }
+
+  protected function addToolbar()
+  {
+    
+	
+	JFactory::getApplication()->input->set('hidemainmenu', true);
+
+    JToolbarHelper::title(JText::_('COM_CLUBMANAGER_MANAGER_REGISTER'), '');
+
+    JToolbarHelper::save('register.save');
+
+    if (empty($this->item->matchID))
+    {
+      JToolbarHelper::cancel('register.cancel');
+    }
+    else
+    {
+      JToolbarHelper::cancel('register.cancel', 'JTOOLBAR_CLOSE');
+    }
+
+	//generate the Html and return the toolbar
+	return JToolBar::getInstance('toolbar')->render();
+  }
+
+  private function includeAdminEnv()
+ 	{
+ 		// load the language files for the admin messages as well
+ 		$language	= JFactory::getLanguage();
+ 		$language->load('joomla', JPATH_ADMINISTRATOR, null, true);
+ 		$language->load('com_clubmanager', JPATH_ADMINISTRATOR, null, true);
+ 
+ 		JLoader::register('JToolBarHelper',   JPATH_ADMINISTRATOR . '/includes/toolbar.php');
+ 		
+  	}
+}
